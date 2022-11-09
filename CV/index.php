d26d55b81d98b004f8468838cac4c54674f975ac
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-08 19:46:30 -0500

    partials-head-scripts

--- CV/index.php
+++ CV/index.php
@@ -1,19 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!----css bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!----end css bootstrap-->
-    <link rel="stylesheet" href="css/mystyle.css">
-    <!----script fontawesome-->
-    <script src="https://kit.fontawesome.com/80573cd677.js" crossorigin="anonymous"></script>
-    <!----end script fontawesome-->
-
-    <title>Template</title>
-</head>
+<?php include 'partials/head.php'; ?>
+<title>Inicio</title>
 <body>
   <?php include 'partials/navbar.php'; ?>
     <div class="container-fluid">
@@ -24,7 +12,7 @@
                 <?php include 'partials/carrousel.php'; ?>              
               </div>
               <div class="col-2">
-              <?php include 'partials/menu_lateral.php'; ?>     
+                <?php include 'partials/menu_lateral.php'; ?>     
               </div>
           </div>
           <div class="row">
@@ -34,15 +22,15 @@
               <div class="col-6">
                 <?php include 'partials/menu_categorias.php'; ?> 
               </div>
-            </div>
+          </div>
         </div>
          <?php include 'partials/footer.php'; ?>
       </div>
       <!-- Modal -->
-     <?php include 'partials/modal_contacto.php'; ?>
+      <?php include 'partials/modal_contacto.php'; ?>
     </div>
  <!----script js bootstrap-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<?php include 'partials/scripts.php'; ?>
 <!----end script js bootstrap-->
 </body>
 </html>
