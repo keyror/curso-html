a64b3135c38d6aebc8cd819e950ed732fe8d0e35
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-08 20:22:38 -0500

    Update navbar.php

--- CV/partials/navbar.php
+++ CV/partials/navbar.php
@@ -1,9 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow rounded  justify-content-center">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.html">
-      <img src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png" alt="" width="40" height="32" class="d-inline-block align-text-bottom">
-      Mi Página
-    </a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow rounded justify-content-center">
+  <div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,15 +8,21 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
-        <li>
+        <li class="nav-item">
           <a class="nav-link" href="">Mis Estudios</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Experiencia Laboral</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="#">Mis Habilidades</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cursos Realizados</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" type="button" data-bs-toggle="modal" href="#contacto">Contactame</a>
         </li>
       </ul>
-    </div>
+  </div>
 </nav>
