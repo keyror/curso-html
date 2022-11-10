a36922cb08f716928f1ccdf06f43c9e7f668fe29
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-09 19:05:08 -0500

    added portada

--- CV/index.php
+++ CV/index.php
@@ -4,33 +4,46 @@
 <title>Inicio</title>
 <body>
   <?php include 'partials/navbar.php'; ?>
-    <div class="container-fluid">
-      <div class="card mt-4 shadow rounded">
-        <div class="card-body">
-          <div class="row">
-              <div class="col-10">
-                <?php include 'partials/carrousel.php'; ?>              
+  <div class="container-fluid">
+    <div class="card mt-4 shadow rounded">
+      <div class="card-body">
+        <div class="row bg-primary bg-gradient rounded text-white">
+          <div class="col">
+            <section class="py-5 text-center container">
+              <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                  <h1 class="fw-light">Camilo Mancipe</h1>
+                  <p>Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                  <p>
+                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
+                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                  </p>
+                </div>
               </div>
-              <div class="col-2">
-                <?php include 'partials/menu_lateral.php'; ?>     
-              </div>
+            </section>
           </div>
-          <div class="row">
-              <div class="col-4">
-                <?php include 'partials/accordion_menu.php'; ?> 
-              </div>
-              <div class="col-6">
-                <?php include 'partials/menu_categorias.php'; ?> 
-              </div>
+          <!--- <div class="col-2">
+                <?php // include 'partials/menu_lateral.php'; 
+                ?>     
+              </div> --->
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <?php include 'partials/accordion_menu.php'; ?>
+          </div>
+          <div class="col-6">
+            <?php include 'partials/menu_categorias.php'; ?>
           </div>
         </div>
-         <?php include 'partials/footer.php'; ?>
       </div>
-      <!-- Modal -->
-      <?php include 'partials/modal_contacto.php'; ?>
+      <?php include 'partials/footer.php'; ?>
     </div>
- <!----script js bootstrap-->
-<?php include 'partials/scripts.php'; ?>
-<!----end script js bootstrap-->
+    <!-- Modal -->
+    <?php include 'partials/modal_contacto.php'; ?>
+  </div>
+  <!----script js bootstrap-->
+  <?php include 'partials/scripts.php'; ?>
+  <!----end script js bootstrap-->
 </body>
+
 </html>
--- CV/partials/navbar.php
+++ CV/partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow rounded justify-content-center">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow justify-content-center">
   <div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -6,22 +6,22 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link  bg-gray mx-2 rounded border-2 active" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link border-start mx-4 rounded border-2 active" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-start border-light mx-2 border-3 rounded" href="">Mis Estudios</a>
+          <a class="nav-link border-start border-light mx-4 border-3 rounded" href="">Mis Estudios</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-start border-light mx-2 border-3 rounded" href="#">Experiencia Laboral</a>
+          <a class="nav-link border-start border-light mx-4 border-3 rounded" href="#">Experiencia Laboral</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-start border-light mx-2 border-3 rounded" href="#">Mis Habilidades</a>
+          <a class="nav-link border-start border-light mx-4 border-3 rounded" href="#">Mis Habilidades</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-start border-light mx-2 border-3 rounded" href="#">Cursos Realizados</a>
+          <a class="nav-link border-start border-light mx-4 border-3 rounded" href="#">Cursos Realizados</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-start border-light mx-2 border-3 rounded" type="button" data-bs-toggle="modal" href="#contacto">Contactame</a>
+          <a class="nav-link border-start border-light mx-4 border-3 rounded" type="button" data-bs-toggle="modal" href="#contacto">Contactame</a>
         </li>
       </ul>
   </div>
