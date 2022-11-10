f9471cb67ce073048a45341bbe063d1c10d8d726
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-09 19:40:24 -0500

    added img

--- CV/partials/navbar.php
+++ CV/partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow justify-content-center">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top shadow justify-content-center">
   <div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
