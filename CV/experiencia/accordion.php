7562f98bb310ef8b926e89ebfe2fd74e4f4574d7
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-11 19:44:21 -0500

    Update accordion.php

--- CV/experiencia/accordion.php
+++ CV/experiencia/accordion.php
@@ -7,35 +7,133 @@
     </h2>
     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
       <div class="accordion-body text-dark">
-        <p><strong>Empresa: </strong> Lorem ipsum dolor sit amet.</p>
-        <p><strong>Desde: </strong> 2016  <strong>hasta: </strong> 2018</p>
-        <p><strong>Tel: </strong> 326587877</p>
-        <p><strong>Cargo: </strong> Desarollador Junior</p>
-        <p>
-         <strong>Funciones: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima animi atque velit voluptate magnam quidem ut saepe hic deserunt?
-        </p>
+        <div class="row">
+            <div class="col">
+                <p><strong>Empresa: </strong> Lorem ipsum dolor sit amet.</p>
+                <p><strong>Desde: </strong> 2016  <strong>hasta: </strong> 2018</p>
+                <p><strong>Tel: </strong> 326587877</p>
+                <p><strong>Cargo: </strong> Desarollador Junior</p>
+                <p>
+                <strong>Funciones: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima animi atque velit voluptate magnam quidem ut saepe hic deserunt?
+            </p>
+            </div>
+            <div class="col">
+                <div class="alert alert-info" role="alert"> 
+                        <strong>Trabajo en equipo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped " role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Liderazgo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Adaptación: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 80%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Resolución de problemas:</strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Puntualidad: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingTwo"> 
       <button class="accordion-button collapsed bg-secondary bg-gradient" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-        Accordion Item #2
+        Desarollador Semi Senior
       </button>
     </h2>
     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
       <div class="accordion-body text-dark">
+        <div class="row">
+            <div class="col">
+                <p><strong>Empresa: </strong> Lorem ipsum dolor sit amet.</p>
+                <p><strong>Desde: </strong> 2016  <strong>hasta: </strong> 2018</p>
+                <p><strong>Tel: </strong> 326587877</p>
+                <p><strong>Cargo: </strong> Desarollador Semi Senior</p>
+                <p>
+                <strong>Funciones: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima animi atque velit voluptate magnam quidem ut saepe hic deserunt?
+                </p>
+            </div>
+            <div class="col">
+                    <div class="alert alert-info" role="alert"> 
+                        <strong>Trabajo en equipo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped " role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Liderazgo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Adaptación: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 80%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Resolución de problemas:</strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Puntualidad: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingThree">
       <button class="accordion-button collapsed bg-secondary bg-gradient" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-        Accordion Item #3
+       Desarollador Senior
       </button>
     </h2>
     <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingThree">
       <div class="accordion-body text-dark">
+      <div class="row">
+            <div class="col">
+                <p><strong>Empresa: </strong> Lorem ipsum dolor sit amet.</p>
+                <p><strong>Desde: </strong> 2016  <strong>hasta: </strong> 2018</p>
+                <p><strong>Tel: </strong> 326587877</p>
+                <p><strong>Cargo: </strong>  Desarollador Senior</p>
+                <p>
+                <strong>Funciones: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima animi atque velit voluptate magnam quidem ut saepe hic deserunt?
+                </p>
+            </div>
+            <div class="col">
+                <div class="alert alert-info" role="alert"> 
+                        <strong>Trabajo en equipo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped " role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Liderazgo: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Adaptación: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 80%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Resolución de problemas:</strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <strong>Puntualidad: </strong>
+                        <div class="progress my-1 shadow-sm">
+                            <div class="progress-bar progress-bar-striped role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
