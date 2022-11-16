822d302c5606c36d8b11a76a44f8bef7637f991c
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-15 19:21:41 -0500

    Update list_group.php

--- CV/habilidades/list_group.php
+++ CV/habilidades/list_group.php
@@ -1,24 +1,21 @@
-<li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">
-        <img class="img-fluid" src="img/php.png" alt="php">
-            PHP
-      </div>
+
+<ul class="list-group">
+  <li class="list-group-item d-inline">
+    <div class="row">
+        <div class="col-2">
+            <img class="img-fluid" width="70%" src="img/php.png" alt="php">
+        </div>
+        <div class="col-10 align-self-center">
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                    25%
+                </div>
+            </div>
+        </div>
     </div>
-    <span class="badge bg-primary rounded-pill">14</span>
   </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Cras justo odio
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Cras justo odio
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-</ol>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
