7d2272c7834dfa0410e09df4f4b9d2f8630ff3ea
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-15 20:08:14 -0500

    Update list_group.php

--- CV/habilidades/list_group.php
+++ CV/habilidades/list_group.php
@@ -5,7 +5,7 @@
         <div class="col">
             <img class="img-fluid" src="img/php.png" alt="php">
         </div>
-        <div class="col align-self-center">
+        <div class="col align-self-center text-center">
            <span class="badge bg-primary"> <h5>4 Años</h5>  </span>
         </div>
         <div class="col-8 align-self-center">
@@ -22,7 +22,7 @@
         <div class="col">
             <img class="img-fluid" src="https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png" alt="html">
         </div>
-        <div class="col align-self-center">
+        <div class="col align-self-center text-center">
            <span class="badge bg-primary"> <h5>5 Años</h5>  </span>
         </div>
         <div class="col-8 align-self-center">
@@ -39,7 +39,7 @@
         <div class="col">
             <img class="img-fluid" src="https://w7.pngwing.com/pngs/696/424/png-transparent-logo-css-css3-thumbnail.png" alt="css">
         </div>
-        <div class="col align-self-center">
+        <div class="col align-self-center text-center">
            <span class="badge bg-primary"> <h5>5 Años</h5>  </span>
         </div>
         <div class="col-8 align-self-center">
