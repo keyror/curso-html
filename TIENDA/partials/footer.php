f806ccc0e37e9114f1dbf0aa7d03b417eff1b32f
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-17 19:14:12 -0500

    added tienda

---
+++ TIENDA/partials/footer.php
@@ -0,0 +1,3 @@
+<div class="card-footer text-center">
+    <small>Derechos reservados 2022</small>
+</div>
---
+++ TIENDA/partials/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow justify-content-center">
+  <div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded "></span> Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mis_estudios.php"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded"></span>Estudios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="experiencia_laboral.php"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded"></span> Experiencia Laboral</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="habilidades.php"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded"></span>Habilidades</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cursos_realizados.php"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded"></span> Cursos Realizados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" type="button" data-bs-toggle="modal" href="#contacto"><span style="height:2px !important ;" class="border border-secondary border-2 bg-secondary text-secondary mx-2 rounded"></span> Contactame</a>
+        </li>
+      </ul>
+  </div>
+</nav>
