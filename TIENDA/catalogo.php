4697d5874c4704ce1e2239a6bb2bf186c90b32c5
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-17 20:06:16 -0500

    added serach

--- TIENDA/catalogo.php
+++ TIENDA/catalogo.php
@@ -8,12 +8,15 @@
       <div class="card-body">
         <div class="row rounded text-white mx-1">
           <div class="col">
-            <form class="d-flex">
-                <input type="number" class="form-control" placeholder="200.000">
-                <input type="number" class="form-control mx-2" placeholder="8.000.000">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div class="card-header bg-secondary shadow rounded">
+                <form class="d-flex">
+                    <input type="number" class="form-control" placeholder="200.000">
+                    <span class="badge bg-primary mx-1 align-self-center"><strong> =< </strong></span>
+                    <input type="number" class="form-control mx-2" placeholder="8.000.000">
+                    <input class="form-control me-2" type="search" placeholder="Busca tu producto" aria-label="Search">
+                    <button class="btn btn-outline-dark" type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
           </div>
         </div>
       </div>
