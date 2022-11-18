ddbf1bd41b7a9f4d6b439b6729cf8c856481574b
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-17 19:41:08 -0500

    Update cards.php

--- TIENDA/inicio/cards.php
+++ TIENDA/inicio/cards.php
@@ -4,7 +4,7 @@
             <h4 class="card-title">Redmi 10A</h4>
         </div>
         <div class="card-body">
-            <img class="img-fluid rounded shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/24ccd3d7-2d6e-4144-bba6-31b21b922411___3720b7a1d70d0c1e5073c3cd3a014c1f.jpg" alt="">
+            <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/24ccd3d7-2d6e-4144-bba6-31b21b922411___3720b7a1d70d0c1e5073c3cd3a014c1f.jpg" alt="">
         </div>
         <div class="card-footer">
             <a href="" role="button" class="btn btn-success"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
@@ -18,7 +18,7 @@
             <h4 class="card-title">Redmi 10C</h4>
         </div>
         <div class="card-body">
-            <img class="img-fluid rounded shadow" src="" alt="">
+            <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
         </div>
         <div class="card-footer">
             <a href="" role="button" class="btn btn-success"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
@@ -32,7 +32,7 @@
             <h4 class="card-title">Redmi Note 11S</h4>
         </div>
         <div class="card-body">
-            <img class="img-fluid rounded shadow" src="" alt="">
+            <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/74db37cc-468c-4243-b042-fe52a7d63029___8b446a06f0d1ea4e48920efd7acc6451.jpg" alt="">
         </div>
         <div class="card-footer">
             <a href="" role="button" class="btn btn-success"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
