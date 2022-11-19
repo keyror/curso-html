06a0d31887abddc7d622c588d072e981d99b8cd9
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-18 19:55:08 -0500

    Update listado_productos.php

--- TIENDA/catalogo/listado_productos.php
+++ TIENDA/catalogo/listado_productos.php
@@ -9,12 +9,24 @@
             </div>
             <div class="card-footer">
             <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
             </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card shadow rounded text-center">
+            <div class="card-header">
+                <h4 class="card-title">Redmi 10C</h4>
+            </div>
+            <div class="card-body">
+                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
+            </div>
+            <div class="card-footer">
                  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
@@ -29,74 +41,8 @@
             </div>
             <div class="card-footer">
                  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card shadow rounded text-center">
-            <div class="card-header">
-                <h4 class="card-title">Redmi 10C</h4>
-            </div>
-            <div class="card-body">
-                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
-            </div>
-            <div class="card-footer">
-                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <div class="card shadow rounded text-center">
-            <div class="card-header">
-                <h4 class="card-title">Redmi 10C</h4>
-            </div>
-            <div class="card-body">
-                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
-            </div>
-            <div class="card-footer">
-                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card shadow rounded text-center">
-            <div class="card-header">
-                <h4 class="card-title">Redmi 10C</h4>
-            </div>
-            <div class="card-body">
-                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
-            </div>
-            <div class="card-footer">
-                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card shadow rounded text-center">
-            <div class="card-header">
-                <h4 class="card-title">Redmi 10C</h4>
-            </div>
-            <div class="card-body">
-                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
-            </div>
-            <div class="card-footer">
-                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
@@ -113,8 +59,8 @@
             </div>
             <div class="card-footer">
                  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
@@ -129,8 +75,8 @@
             </div>
             <div class="card-footer">
                  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
@@ -145,8 +91,58 @@
             </div>
             <div class="card-footer">
                  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a role="button" href="#" class="btn btn-danger btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
-                    <a role="button" href="#" class="btn btn-warning btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <div class="card shadow rounded text-center">
+            <div class="card-header">
+                <h4 class="card-title">Redmi 10C</h4>
+            </div>
+            <div class="card-body">
+                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
+            </div>
+            <div class="card-footer">
+                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card shadow rounded text-center">
+            <div class="card-header">
+                <h4 class="card-title">Redmi 10C</h4>
+            </div>
+            <div class="card-body">
+                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
+            </div>
+            <div class="card-footer">
+                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card shadow rounded text-center">
+            <div class="card-header">
+                <h4 class="card-title">Redmi 10C</h4>
+            </div>
+            <div class="card-body">
+                <img class="img-fluid rounded-3 shadow" src="https://xiaomicolombia.vtexassets.com/assets/vtex.file-manager-graphql/images/ee09d3b0-a592-459d-813c-7ed490351248___242a1505f9b75dd01e1d9563456dc5a7.jpg" alt="">
+            </div>
+            <div class="card-footer">
+                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <a role="button" href="#" class="btn btn-success btn-sm"><i class="fa-solid fa-dollar-sign"></i> Comprar</a>
+                    <a role="button" href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-cart-plus"></i></a>
                 </div>
             </div>
         </div>
