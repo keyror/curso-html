8f23139b01066f86b5e1597ebd63586edc4fac45
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-21 19:29:47 -0500

    Update detalle_productos.php

--- TIENDA/detalle/detalle_productos.php
+++ TIENDA/detalle/detalle_productos.php
@@ -22,7 +22,36 @@
         </div>
     </div>
     <div class="col-6">
-
+        <div id="carouselExampleIndicators" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://xiaomicolombia.vtexassets.com/arquivos/ids/156748-1200-auto?v=637740865965500000&width=1200&height=auto&aspect=true" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://xiaomicolombia.vtexassets.com/arquivos/ids/156746-1200-auto?v=637740865642700000&width=1200&height=auto&aspect=true" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://xiaomicolombia.vtexassets.com/arquivos/ids/156750-1200-auto?v=637740866266330000&width=1200&height=auto&aspect=true" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://xiaomicolombia.vtexassets.com/arquivos/ids/156751-1200-auto?v=637740866415900000&width=1200&height=auto&aspect=true" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
     </div>
     <div class="col-4">
 
