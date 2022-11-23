53b3854ecca8147c562d50e4554b40ae1f9a2924
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-22 20:18:08 -0500

    Update carrito_compras.php

--- TIENDA/carrito/carrito_compras.php
+++ TIENDA/carrito/carrito_compras.php
@@ -1,10 +1,11 @@
-<table class="table">
+<table class="table shadow rounded-3 table-dark table-striped table-hover">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Producto</th>
+      <th scope="col">Cantidad</th>
+      <th scope="col">Precio unidad</th>
+      <th scope="col">Total</th>
+      <th scope="col">Acciones</th>
     </tr>
   </thead>
   <tbody>
@@ -13,17 +14,31 @@
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
+      <td>
+       <a href="#" class="btn btn-danger" role="button"> - </a>
+       <a href="#" class="btn btn-primary" role="button"> + </a>
+      </td>
     </tr>
     <tr>
       <th scope="row">2</th>
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
+      <td>
+        <a href="#" class="btn btn-danger" role="button"> - </a>
+        <a href="#" class="btn btn-primary" role="button"> + </a>
+      </td>
     </tr>
     <tr>
       <th scope="row">3</th>
       <td colspan="2">Larry the Bird</td>
       <td>@twitter</td>
+      <td>
+        <a href="#" class="btn btn-danger" role="button"> - </a>
+        <a href="#" class="btn btn-primary" role="button"> + </a>
+      </td>
     </tr>
   </tbody>
 </table>
+<button class="btn btn-danger" type="button">Cancelar</button>
+<button class="btn btn-success" type="button">Pagar</button>
