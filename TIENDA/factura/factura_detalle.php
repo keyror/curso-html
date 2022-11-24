55d3bdd19ea7c28c77aed0785d9ef9f8c4db400b
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-24 18:53:40 -0500

    Update factura_detalle.php

--- TIENDA/factura/factura_detalle.php
+++ TIENDA/factura/factura_detalle.php
@@ -1,18 +1,43 @@
 <div class="card">
     <div class="card-body">
         <div class="card-header shadow bg-secondary rounded text-white">
-                <div class="row">
-                    <div class="col-4">
-                        <img width="30%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_GupguAOOCkmikcijI_Bq81_K8aRmKmcNj75lOWNKFVoTUAn6uQm1L-7rjJljLqEGkC8&amp;usqp=CAU" class="img-fluid rounded" alt="...">
-                        <strong>Xiaomi Store</strong> 
-                    </div>
-                    <div class="col-8">
-                        <p class="text-center pt-3">
-                           | Ubicación : bogota
-                           | Dirección : crr 177 # 50 105
-                        </p>
-                    </div>
+            <div class="row">
+                <div class="col-4">
+                    <img width="30%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_GupguAOOCkmikcijI_Bq81_K8aRmKmcNj75lOWNKFVoTUAn6uQm1L-7rjJljLqEGkC8&amp;usqp=CAU" class="img-fluid rounded" alt="...">
+                    <strong>Xiaomi Store</strong>
                 </div>
+                <div class="col-8">
+                    <p class="text-center pt-3">
+                        | Ubicación : bogota
+                        | Dirección : crr 177 # 50 105
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th>Nombres:</th>
+                            <td>Eddison camilo</td>
+                            <th>Apellidos</th>
+                            <td>Mancipe diaz</td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     <div class="card-footer">
