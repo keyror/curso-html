34d76d08cd70442f62a94c3e9f24878562406d73
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-23 19:24:35 -0500

    added

---
+++ TIENDA/factura.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'partials/head.php'?>
+<title>Document</title>
+<body>
+<?php include 'partials/navbar.php'?>
+    <div class="card mt-4 shadow rounded">
+      <div class="card-body">
+        <div class="row rounded text-white mx-1">
+          <div class="col">
+           <?php include 'factura/factura_detalle.php';?>
+          </div>
+        </div>
+      </div>
+      <?php include 'partials/footer.php'; ?>
+    </div>
+<?php include 'partials/scripts.php'; ?>
+</body>
+</html>
