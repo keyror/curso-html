5c5fda2be38609250e58bf46a2ee1f23bd503316
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-23 19:19:03 -0500

    Update carrito_compras.php

--- TIENDA/carrito/carrito_compras.php
+++ TIENDA/carrito/carrito_compras.php
@@ -1,44 +1,57 @@
-<table class="table shadow rounded-3 table-dark table-striped table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Producto</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Precio unidad</th>
-      <th scope="col">Total</th>
-      <th scope="col">Acciones</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>
-       <a href="#" class="btn btn-danger" role="button"> - </a>
-       <a href="#" class="btn btn-primary" role="button"> + </a>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>
+<div class="card-header bg-secondary shadow my-4 rounded text-center">
+  <h4><i class="fa-solid fa-cart-shopping"></i> Carrito de Compras</h4>
+</div>
+<div class="table-responsive"><center>
+  <table class="table shadow table-striped table-hover text-center" style="border-radius: 20px !important;">
+    <thead>
+      <tr>
+        <th scope="col">Producto</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">Precio unidad</th>
+        <th scope="col">Total</th>
+        <th scope="col">Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <img width="20%" class="img-fluid rounded-3 shadow" src="https://m.media-amazon.com/images/I/5185wCvMLxL._CR0,115,890,890_UX256.jpg" alt="">
+        </td>
+        <td>2</td>
+        <td>$ 1.000.000</td>
+        <td>$ 2.000.000</td>
+        <td>
         <a href="#" class="btn btn-danger" role="button"> - </a>
         <a href="#" class="btn btn-primary" role="button"> + </a>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <img width="20%" class="img-fluid rounded-3 shadow" src="https://m.media-amazon.com/images/I/5185wCvMLxL._CR0,115,890,890_UX256.jpg" alt="">
+        </td>
+        <td>1</td>
+        <td>$ 1.000.000</td>
+        <td>$ 1.000.000</td>
+        <td>
+          <a href="#" class=" btn btn-danger" role="button"> - </a>
+          <a href="#" class="btn btn-primary" role="button"> + </a>
+        </td>
+      </tr>
+      <tr>
       <td>
-        <a href="#" class="btn btn-danger" role="button"> - </a>
-        <a href="#" class="btn btn-primary" role="button"> + </a>
-      </td>
-    </tr>
-  </tbody>
-</table>
-<button class="btn btn-danger" type="button">Cancelar</button>
-<button class="btn btn-success" type="button">Pagar</button>
+          <img width="20%" class="img-fluid rounded-3 shadow" src="https://m.media-amazon.com/images/I/5185wCvMLxL._CR0,115,890,890_UX256.jpg" alt="">
+        </td>
+        <td>1</td>
+        <td>$ 1.000.000</td>
+        <td>$ 1.000.000</td>
+        <td>
+          <a href="#" class="btn btn-danger" role="button"> - </a>
+          <a href="#" class="btn btn-primary" role="button"> + </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <a class="btn btn-danger" href="catalogo.php" role="button">Cancelar</a>
+  <a href="" class="btn btn-success" role="button">Pagar</a>
+  </center>
+</div>
