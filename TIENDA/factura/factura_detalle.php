bd2106f0343391b869318286106f4f4b3bc892b9
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-23 19:56:29 -0500

    Update factura_detalle.php

--- TIENDA/factura/factura_detalle.php
+++ TIENDA/factura/factura_detalle.php
@@ -1,15 +1,15 @@
 <div class="card">
     <div class="card-body">
-        <div class="card-header">
+        <div class="card-header shadow bg-secondary rounded text-white">
                 <div class="row">
-                    <div class="col-2">
-                        <img width="60%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_GupguAOOCkmikcijI_Bq81_K8aRmKmcNj75lOWNKFVoTUAn6uQm1L-7rjJljLqEGkC8&amp;usqp=CAU" class="img-fluid rounded-start" alt="...">
+                    <div class="col-4">
+                        <img width="30%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_GupguAOOCkmikcijI_Bq81_K8aRmKmcNj75lOWNKFVoTUAn6uQm1L-7rjJljLqEGkC8&amp;usqp=CAU" class="img-fluid rounded" alt="...">
                         <strong>Xiaomi Store</strong> 
                     </div>
-                    <div class="col-10">
+                    <div class="col-8">
                         <p class="text-center pt-3">
-                            Ubicación : bogota
-                            Dirección : crr 177 # 50 105
+                           | Ubicación : bogota
+                           | Dirección : crr 177 # 50 105
                         </p>
                     </div>
                 </div>
