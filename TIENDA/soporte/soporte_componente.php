314b374bebdd6a45c19dbdc8394aee3ca83d0c9e
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-29 20:22:33 -0500

    added

--- TIENDA/soporte/soporte_componente.php
+++ TIENDA/soporte/soporte_componente.php
@@ -3,12 +3,13 @@
     <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true"><i class="fa-solid fa-house-user"></i> Inicio</button>
     <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="fa-solid fa-ticket"></i> Abrir ticket</button>
     <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="fa-solid fa-ticket-simple"></i> Trazabilidad Tickets</button>
-    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="fa-solid fa-map-location-dot"></i> Trazabilidad</button>
+    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="fa-solid fa-map-location-dot"></i> Trazabilidad Pedido</button>
   </div>
 </nav>
 <div class="tab-content text-center" id="nav-tabContent">
     <?php include 'inicio.php'; ?>
     <?php include 'apertura_ticket.php'; ?>
+    <?php include 'trazabilidad_tikects.php'; ?>
     <?php include 'trazabilidad.php'; ?>
 </div>
  
---
+++ TIENDA/soporte/trazabilidad_tikects.php
@@ -0,0 +1,120 @@
+<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact">
+    <div class="card-header bg-secondary shadow rounded text-center text-white mb-4">
+        <h4><i class="fa-solid fa-map-location-dot"></i> Trazabilidad Ticket</h4>
+    </div>
+    <form class="d-flex mb-4">
+        <input class="form-control me-2" type="search" placeholder="Introduce el número de tu ticket" aria-label="Search">
+        <button class="btn btn-success" type="submit">Rastrear</button>
+    </form>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button d-block text-center d-block text-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <i class="fa-solid fa-folder-open"></i> 2022/10/10 - 16:00:00
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Referencia Factura:</strong> A1589</p>
+                    <p><strong>Evidencia: </strong> <img class="img-fluid rounded" width="20%" src="https://www.elcolombiano.com/documents/10157/0/843x562/0c0/0d0/none/11101/INHP/image_content_26308868_20160622195756.jpg" alt=""></p>
+                    <p><strong>Descripción: </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum consectetur rerum aut illo ducimus tempore veritatis cumque distinctio architecto autem.</p>
+                    <p><span class="badge bg-info"> Apertura ticket</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <i class="fa-solid fa-reply"></i> 2022/10/11 - 18:30:00
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Respuesta vendedor : </strong>Su caso sera revisado con unos de nuestros tecnicos</p>
+                    <hr>
+                    <div class="text-muted">
+                        <p><strong>Referencia Factura:</strong> A1589</p>
+                        <p><strong>Evidencia: </strong> <img class="img-fluid rounded" width="20%" src="https://www.elcolombiano.com/documents/10157/0/843x562/0c0/0d0/none/11101/INHP/image_content_26308868_20160622195756.jpg" alt=""></p>
+                        <p><strong>Descripción: </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum consectetur rerum aut illo ducimus tempore veritatis cumque distinctio architecto autem.</p>
+                        <p><span class="badge bg-info"> Respuesta Vendedor</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fa-solid fa-truck-arrow-right"></i> 2022/10/11 - 19:10:00
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Producto :</strong> celular xiaomi redmi note 11</p>
+                    <p><strong>Costo :</strong> $ 1.800.000</p>
+                    <p><strong>Vendedor: </strong> xiaomi</p>
+                    <p><span class="badge bg-info"> El pedido ingreso a la empresa distribuidora</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fa-solid fa-truck-front"></i> 2022/10/15 - 17:10:00
+                </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Producto :</strong> celular xiaomi redmi note 11</p>
+                    <p><strong>Costo :</strong> $ 1.800.000</p>
+                    <p><strong>Vendedor: </strong> xiaomi</p>
+                    <p><span class="badge bg-primary"> El pedido esta con detino a tu ciudad</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fa-solid fa-truck-ramp-box"></i> 2022/10/18 - 14:00:00
+                </button>
+            </h2>
+            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Producto :</strong> celular xiaomi redmi note 11</p>
+                    <p><strong>Costo :</strong> $ 1.800.000</p>
+                    <p><strong>Vendedor: </strong> xiaomi</p>
+                    <p><span class="badge bg-primary"> El pedido se encuentra en distribucción en tu ciudad</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fa-solid fa-triangle-exclamation"></i> 2022/10/18 - 15:10:00
+                </button>
+            </h2>
+            <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Producto :</strong> celular xiaomi redmi note 11</p>
+                    <p><strong>Costo :</strong> $ 1.800.000</p>
+                    <p><strong>Vendedor: </strong> xiaomi</p>
+                    <p><span class="badge bg-warning"> El pedido se va a retrasar</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button d-block text-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseThree">
+                    <i class="fa-solid fa-house"></i> 2022/10/18 - 17:20:00
+                </button>
+            </h2>
+            <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p><strong>Producto :</strong> celular xiaomi redmi note 11</p>
+                    <p><strong>Costo :</strong> $ 1.800.000</p>
+                    <p><strong>Vendedor: </strong> xiaomi</p>
+                    <p><span class="badge bg-success"> El pedido fue entregado</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
