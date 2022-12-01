2901c0582818879466c0f04e920ed60cfb4071a2
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-30 20:37:44 -0500

    Update chat.php

--- TIENDA/soporte/chat.php
+++ TIENDA/soporte/chat.php
@@ -6,7 +6,49 @@
         <div class="card-heder">
            <a href="" class="text-danger"><i class="fa-solid fa-circle-xmark float-end fa-2x m-2"></i></a> 
         </div>
-        <div class="card-body bg-secondary">
+        <div class="card-body bg-secondary" style="height: 500px;overflow-y: scroll;">
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-success shadow" role="alert">
+                        Hola <i class="fa-solid fa-face-smile text-warning"></i>.
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:37 pm</small>
+                    </div>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <div class="alert alert-dark shadow" role="alert">
+                        Hola. Te habla camilo del equipo de soporte, como podemos ayudarte ?.
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:38 pm</small>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-success shadow" role="alert">
+                     Mi nombre es jose, mi equipo movil se averío y necesito una solución. el número de factura es A4545
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:40 pm</small>
+                    </div>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <div class="alert alert-dark shadow" role="alert">
+                    Jose es un gusto para nosotros ayudarte, realizaremos la validación de tú factura 
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:38 pm</small>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col">
                     <div class="alert alert-success shadow" role="alert">
