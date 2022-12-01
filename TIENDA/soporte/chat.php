266b05faa7864e7dce462d86326364402d576aee
keymiro <44626685+keymiro@users.noreply.github.com>
2022-11-30 19:49:39 -0500

    Update chat.php

--- TIENDA/soporte/chat.php
+++ TIENDA/soporte/chat.php
@@ -2,5 +2,74 @@
     <div class="card-header bg-secondary shadow rounded text-center text-white mb-4">
         <h4><i class="fa-solid fa-message"></i> Habla con nosotros</h4>
     </div>
-    
+    <div class="card">
+        <div class="card-body bg-secondary">
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-success shadow" role="alert">
+                        Hola <i class="fa-solid fa-face-smile text-warning"></i>.
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:37 pm</small>
+                    </div>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <div class="alert alert-dark shadow" role="alert">
+                        Hola. Te habla camilo del equipo de soporte, como podemos ayudarte ?.
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:38 pm</small>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-success shadow" role="alert">
+                     Mi nombre es jose, mi equipo movil se averío y necesito una solución. el número de factura es A4545
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:40 pm</small>
+                    </div>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <div class="alert alert-dark shadow" role="alert">
+                    Jose es un gusto para nosotros ayudarte, realizaremos la validación de tú factura 
+                        <br>
+                        <small class="text-dark mt-4 float-end">7:38 pm</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="row">
+                <div class="col-lg-8 col-sm-12 mb-2">
+                    <textarea name="" id="" cols="4" rows="1" class="form-control" placeholder="Introduce tu mensaje"></textarea>
+                </div>
+                <div class="col">
+                    <button class="btn btn-primary" type="button">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </button>
+                    <button class="btn btn-secondary" type="button">
+                        <i class="fa-solid fa-microphone"></i>
+                    </button>
+                    <button class="btn btn-warning" type="button">
+                        <i class="fa-solid fa-face-smile"></i>
+                    </button>
+                    <button class="btn btn-info" type="button">
+                        <i class="fa-solid fa-paperclip"></i>
+                    </button>
+                    <button class="btn btn-dark" type="button">
+                        <i class="fa-solid fa-images"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
